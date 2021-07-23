41f58b00656dc483bfbfbe328fd859efd3424469
ricky kuldeep <rickykldp@gmail.com>
2021-07-23 23:27:34 +0530

    1

---
+++ terraform.tf
@@ -0,0 +1,55 @@
+{
+  "version": 4,
+  "terraform_version": "0.12.21",
+  "serial": 3,
+  "lineage": "2e260e6f-146c-5476-7fb9-3a12f546580f",
+  "outputs": {},
+  "resources": [
+    {
+      "mode": "managed",
+      "type": "aws_launch_template",
+      "name": "test-ami",
+      "provider": "provider.aws",
+      "instances": [
+        {
+          "schema_version": 0,
+          "attributes": {
+            "arn": "arn:aws:ec2:us-east-1:099892843683:launch-template/lt-0fa81ee98004aa77f",
+            "block_device_mappings": [],
+            "capacity_reservation_specification": [],
+            "cpu_options": [],
+            "credit_specification": [],
+            "default_version": 1,
+            "description": "",
+            "disable_api_termination": false,
+            "ebs_optimized": "",
+            "elastic_gpu_specifications": [],
+            "elastic_inference_accelerator": [],
+            "iam_instance_profile": [],
+            "id": "lt-0fa81ee98004aa77f",
+            "image_id": "ami-0b80751d95242a7f1",
+            "instance_initiated_shutdown_behavior": "",
+            "instance_market_options": [],
+            "instance_type": "t2.micro",
+            "kernel_id": "",
+            "key_name": "",
+            "latest_version": 1,
+            "license_specification": [],
+            "monitoring": [],
+            "name": "test-ami20200228141637549400000002",
+            "name_prefix": "test-ami",
+            "network_interfaces": [],
+            "placement": [],
+            "ram_disk_id": "",
+            "security_group_names": null,
+            "tag_specifications": [],
+            "tags": null,
+            "user_data": "",
+            "vpc_security_group_ids": null
+          },
+          "private": "bnVsbA=="
+        }
+      ]
+    }
+  ]
+}
